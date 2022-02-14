6c96bec47562a3a4ab104466e143e64362692486
MaAuWi <95444056+MaAuWi@users.noreply.github.com>
2022-02-14 14:41:27 +0100

    Delete nihma_logo_farbe_transp.tsx

--- nihma_logo_farbe_transp.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="nihma_logo_farbe_transp" tilewidth="32" tileheight="32" tilecount="1550" columns="62">
- <image source="../../../../NIHMA/Vorlagen/Logo/nihma_logo_farbe_transp.PNG" trans="ff00ff" width="2000" height="800"/>
-</tileset>
